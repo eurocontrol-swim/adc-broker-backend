9cb409234c794c0ddf67f7c0ecadb3a6a837022f
GRIGNON Leo <leo.grignon@thalesgroup.com>
2021-12-01 17:06:27 +0100

    User management (get users and post new users)

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
+from django.urls import path, include
 from backend import views
 
 urlpatterns = [
-    url(r'^(?P<path>.*)$', views.index.as_view(), name='index'),
-    url(r'login$', views.auth, name='auth'),
-    url(r'getPublisherPolicies$', views.getPublisherPolicies, name='getPublisherPolicies'),
+    # url(r'accounts/', include('django.contrib.auth.urls')),
+    # url(r'login$', views.auth, name='auth'),
+    url(r'api/postUser', views.postUser, name='postUser'),
+    url(r'api/getUsers', views.getUsers, name='getUsers'),
+
+    # catch-all pattern for compatibility with the Angular routes. This must be last in the list.
+    url(r'^(?P<path>.*)$', views.index, name='index'),
 ]
