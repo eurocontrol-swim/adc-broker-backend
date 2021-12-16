75b0b3b0f934292f1cfff6c882ce4198ab4de885
GRIGNON Leo <leo.grignon@thalesgroup.com>
2021-12-16 15:50:10 +0100

    views_user, beginning of the password reset

--- adc_backend/settings.py
+++ adc_backend/settings.py
@@ -136,6 +136,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 BROKER_AMQP_URL = 'amqps://adcbroker:5771/'
 
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- backend/urls.py
+++ backend/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls import url
 from django.urls import path, include
-from backend import views
+from backend import views, views_user
 
 urlpatterns = [
     # url(r'accounts/', include('django.contrib.auth.urls')),
-    # url(r'login$', views.auth, name='auth'),
-    url(r'api/postUser', views.postUser, name='postUser'),
-    url(r'api/getUsers', views.getUsers, name='getUsers'),
-    url(r'api/deleteUser', views.deleteUser, name='deleteUser'),
+    url(r'auth$', views_user.auth, name='auth'),
+    url(r'logout$', views_user.logout, name='login'),
+
+    url(r'accounts/', include('django.contrib.auth.urls')),
+    url(r'password_reset', views_user.password_reset_request, name="password_reset"),
+
+    url(r'api/postUser', views_user.postUser, name='postUser'),
+    url(r'api/getUsers', views_user.getUsers, name='getUsers'),
+    url(r'api/deleteUser', views_user.deleteUser, name='deleteUser'),
 
     url(r'api/postDataCatalogue', views.postDataCatalogue, name='postDataCatalogue'),
     url(r'api/getDataCatalogue', views.getDataCatalogue, name='getDataCatalogue'),
